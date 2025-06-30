de11c9e17ae647763b923c4f5a1802e1bb02a2dc
Grishma Dhakal <dhakalgrishma5@gmail.com>
2025-06-30 21:39:07 +0545

    fix issues in blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,13 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-const postCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        date: z.string(),
-    }),
+const posts = defineCollection({
+    loader: glob({ base: "./src/content/posts", pattern: "**/*.{md,mdx}" }),
+    schema: () =>
+        z.object({
+            title: z.string(),
+            date: z.coerce.date(),
+        }),
 });
 
-export const collections = {
-    posts: postCollection,
-};
+export const collections = { posts };
