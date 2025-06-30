8dab254285bfd582afb363ba9dff8876c075471a
Grishma Dhakal <dhakalgrishma5@gmail.com>
2025-06-30 09:44:35 +0545

    project, font added and header fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Geist Mono",
+            cssVariable: "--font-geist-mono"
+        }],
+    },
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
